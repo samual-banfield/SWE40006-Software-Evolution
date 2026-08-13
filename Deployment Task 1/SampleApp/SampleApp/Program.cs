252cd75a8d717ae5cc63ff09acb872f1e5141840
samual-banfield <samualbanfield@gmail.com>
2026-08-13 22:04:22 +1000

    Task 1.1 Complete

---
+++ Deployment Task 1/SampleApp/SampleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deplyment Task 1: Pass Task");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
