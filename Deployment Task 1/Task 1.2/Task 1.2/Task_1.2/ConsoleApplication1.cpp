724590da9e476b3fd0886ff1af0b747fb5b89697
samual-banfield <samualbanfield@gmail.com>
2026-08-16 15:34:54 +1000

    Task 1.2 Completed

---
+++ Deployment Task 1/Task 1.2/Task 1.2/Task_1.2/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <conio.h>
+
+int main()
+{
+    std::cout << "Hello from HelloApp!\n";
+    std::cout << "Press any key to exit...";
+    _getch();
+    return 0;
+}
