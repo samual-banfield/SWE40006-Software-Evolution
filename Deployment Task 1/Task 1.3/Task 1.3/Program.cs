bfe4fa743d92fca93d0c9ceabf0f190abf63150e
samual-banfield <samualbanfield@gmail.com>
2026-08-18 20:26:17 +1000

    Task 1.3 Complete

---
+++ Deployment Task 1/Task 1.3/Task 1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+class Program
+{
+    const int VK_LBUTTON = 0x01;
+
+    [DllImport("user32.dll")]
+    static extern short GetAsyncKeyState(int vKey);
+
+    static void Main()
+    {
+        Console.WriteLine("Listening for left clicks. Ctrl+C to quit.");
+
+        bool wasDown = false;
+        while (true)
+        {
+            bool isDown = (GetAsyncKeyState(VK_LBUTTON) & 0x8000) != 0;
+
+            if (isDown && !wasDown)
+                Console.WriteLine("Left click pressed");
+
+            wasDown = isDown;
+            Thread.Sleep(10);
+        }
+    }
+}
